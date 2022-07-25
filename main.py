afaf244a60741bd303cca4e7a1c25114007c9841
Roland Kahlert <roland.kahlert@pwc.com>
2022-07-25 22:40:17 +0200

    show real weather

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import configparser
 import logging
+import os
 import time
+from logging.config import fileConfig
 
+import requests
 from luma.core.interface.serial import spi, noop
 from luma.core.legacy import text
 from luma.core.legacy.font import proportional, CP437_FONT
 from luma.core.render import canvas
 from luma.led_matrix.device import max7219
 
+fileConfig('logging.ini')
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+
+def current_temperature():
+    api_key = os.environ['WEATHER_API_KEY']
+    lat, lon = config['weather']['latitude'], config['weather']['longitude']
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={api_key}')
+    if not response.ok:
+        return 'ERROR'
+    temp_value = response.json()['main']['temp']
+    return f'{temp_value:.0f} C'
 
 
 if __name__ == "__main__":
@@ -26,11 +40,10 @@ if __name__ == "__main__":
         logging.info('Falling back to emulator')
     # start show
     try:
-        temperature = '22 C'
-
-        with canvas(device) as draw:
-            text(draw, (4, 1), temperature, fill='white', font=proportional(CP437_FONT))
-        time.sleep(5)
+        while True:
+            with canvas(device) as draw:
+                text(draw, (4, 1), current_temperature(), fill='white', font=proportional(CP437_FONT))
+            time.sleep(60)
 
     except KeyboardInterrupt:
         logging.info('Shutting down')
