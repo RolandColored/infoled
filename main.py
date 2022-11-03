9040aeab3df06930e57e0c056bafd93529963789
Roland Kahlert <roland.kahlert@pwc.com>
2022-11-03 16:59:10 +0100

    reduce contrast

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ if __name__ == "__main__":
     try:
         serial = spi(port=0, device=0, gpio=noop())
         device = max7219(serial, cascaded=4, block_orientation=90, rotate=2)
+        device.contrast(60)
         print('Created device')
     except ModuleNotFoundError:
         from luma.emulator.device import pygame
 
         device = pygame(width=config['led'].getint('width'), height=config['led'].getint('height'),
                         mode='1', transform='led_matrix')
-        device.contrast(60)
         logging.info('Falling back to emulator')
 
     # start show
